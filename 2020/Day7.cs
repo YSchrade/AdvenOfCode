9cb1f85acab626c88fa0c1a723ec18f23b12aee7
Yannick Schrade <yannick1218@gmail.com>
2020-12-10 19:39:24 +0100

    Added Day8

---
+++ 2020/Day7.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    internal class Day7 : IDay
+    {
+        private readonly string[] _lines;
+        private readonly Dictionary<string, List<string>> _graph1 = new Dictionary<string, List<string>>();
+        private readonly Dictionary<string, List<(string, int)>> _graph2 = new Dictionary<string, List<(string, int )>>();
+
+        public Day7(string path)
+        {
+            _lines = File.ReadAllLines(path);
+        }
+
+
+        public long Solve1()
+        {            
+            foreach (var line in _lines)
+            {
+                var primColor = Regex.Match(line, @"(.+?) bags").Groups[1].Value;
+                var insideColors = Regex.Matches(line, @"(\d+) (.+?) bag").Select(m => m.Groups[2].Value).ToList();
+
+                _graph1.Add(primColor, insideColors);
+            }
+
+            return _graph1.Keys.Sum(k => ContainsGoldBag(k) ? 1 : 0) -1;
+        }
+
+
+        public long Solve2()
+        {
+            
+            foreach (var line in _lines)
+            {
+                var primColor = Regex.Match(line, @"(.+?) bags").Groups[1].Value;
+                var insideColors = Regex.Matches(line, @"(\d+) (.+?) bag")
+                                    .Select(m => (m.Groups[2].Value, int.Parse(m.Groups[1].Value))).ToList();
+
+                _graph2.Add(primColor, insideColors);
+            }
+
+            return CountInside("shiny gold") - 1;
+        }
+
+        private bool ContainsGoldBag(string color) 
+            => color == "shiny gold" || _graph1[color].Any(c => ContainsGoldBag(c));
+
+        private int CountInside(string color)
+        {
+            var erg = 0;
+            
+            foreach ((var name, var count) in _graph2[color])
+                erg += count * CountInside(name);
+
+            return erg + 1;
+        }
+
+    }
+}
---
+++ 2020/Day8.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace AdventOfCode
+{
+    internal class Day8 : IDay
+    {
+
+        private readonly string[] _lines;
+        private readonly List<int> _executedLines = new List<int>();
+
+        public Day8(string path)
+        {
+            _lines = File.ReadAllLines(path);
+        }
+
+        public long Solve1()
+        {
+            var code = new Dictionary<int, (string, int)>();
+
+            for (var i = 0; i < _lines.Length; i++)
+            {
+                var regex = Regex.Match(_lines[i], @"([a-zA-Z]{3})\s([-|+]\d+)");
+                var command = regex.Groups[1].Value;
+                var number = int.Parse(regex.Groups[2].Value);
+                code[i] = (command, number);
+            }
+                
+
+            return ExecuteCode(0, code, out _);
+         
+        }
+
+
+        public long Solve2()
+        {
+            var code = new Dictionary<int, (string cmd, int nbr)>();
+            for (var i = 0; i < _lines.Length; i++)
+            {
+                var regex = Regex.Match(_lines[i], @"([a-zA-Z]{3})\s([-|+]\d+)");
+                var command = regex.Groups[1].Value;
+                var number = int.Parse(regex.Groups[2].Value);
+                code[i] = (command, number);
+            }
+
+            foreach ((var line, var instr) in code.Where(c => c.Value.cmd == "jmp" || c.Value.cmd == "nop"))
+            {
+                var newCode = new Dictionary<int, (string, int)>(code);
+                bool corrupt;
+                var erg = 0;
+
+                if(instr.cmd == "jmp")
+                {
+                    newCode[line] = ("nop", instr.nbr);
+                    erg = ExecuteCode(0, newCode, out corrupt);
+                }
+                else
+                {
+                    newCode[line] = ("jmp", instr.nbr);
+                    erg = ExecuteCode(0, newCode, out corrupt);
+                }
+                
+                if (!corrupt)
+                    return erg;
+
+            }
+
+            return -1;
+        }
+
+
+
+        
+        private int ExecuteCode(int line, Dictionary<int, (string, int)> code, out bool corrupt)
+        {
+            corrupt = true;
+            var acc = 0;
+            if (_executedLines.Contains(line))
+            {
+                _executedLines.Clear();
+                return acc;
+            }
+                
+
+            if(line == code.Count)
+            {
+                corrupt = false;
+                return acc;
+            }
+
+            (var cmd, var nbr) = code[line];
+                
+            _executedLines.Add(line);
+            switch (cmd)
+            {
+                case "nop":
+                    acc += ExecuteCode(++line, code, out corrupt);
+                    break;
+
+                case "jmp":
+                    acc += ExecuteCode(line + nbr, code, out corrupt);
+                    break;
+
+                case "acc":
+                    acc += nbr;
+                    acc += ExecuteCode(++line, code, out corrupt);
+                    break;
+                default:
+                    break;
+            }
+
+            return acc;
+        }
+    }
+}
