e61ec2f2619d6f29638eaac100622c13dcad1b5e
Yannick Schrade <yannick1218@gmail.com>
2020-12-10 19:24:58 +0100

    Added Day1

---
+++ 2020/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using AdventOfCode;
+
+
+
+var path = @"data/day1.txt";
+
+var puzzle = new Day1(path);
+var erg = 0l;
+var s = Stopwatch.StartNew();
+erg = puzzle.Solve1();
+s.Stop();
+
+Console.WriteLine(erg);
+Console.WriteLine(s.Elapsed.TotalMilliseconds);
+
+
