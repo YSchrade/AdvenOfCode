928b018dccb8364c778c63cc3dfa2d079c065b9f
Yannick Schrade <yannick1218@gmail.com>
2020-12-10 19:39:46 +0100

    Added Day9

---
+++ 2020/Day9.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    internal class Day9 : IDay
+    {
+        private readonly string[] _lines;
+
+        public Day9(string path)
+        {
+            _lines = File.ReadAllLines(path);
+        }
+
+        public long Solve1() 
+        {
+            var preamble = 25;
+
+            var numbers = _lines.Select(l => long.Parse(l)).ToArray();
+            for (var i = preamble; i < numbers.Length; i++)
+            {
+                var contains = false;
+                var pref = numbers[(i-preamble)..i];
+                foreach (var num in pref)
+                {
+                    foreach (var num2 in pref.Skip(1))
+                    {
+                        if (num + num2 == numbers[i])
+                        {
+                            contains = true;
+                            break;
+                        }
+                        
+                    }
+
+                    if (contains)
+                        break;
+                }
+
+                if (!contains)
+                    return numbers[i];
+
+                contains = false;
+            }
+            return -1;
+        }
+
+
+        public long Solve2() 
+        {
+            var numbers = _lines.Select(l => long.Parse(l)).ToArray();
+            var selectedNum = Solve1();
+            var index = Array.IndexOf(numbers, selectedNum);
+            var searchNums = numbers[..index].Where(n => n < selectedNum).ToList();
+
+            for (var i = 0; i < searchNums.Count; i++)
+            {
+                var count = searchNums[i];
+                for (var j = i+1; j < searchNums.Count; j++)
+                {
+                    count += searchNums[j];
+
+                    if (count == selectedNum)
+                    {
+                        var range = searchNums.GetRange(i, j - i);
+                        var smallest = range.Min();
+                        var biggest = range.Max();
+                        return biggest + smallest;
+                    }
+                        
+
+                    if (count > selectedNum)
+                        break;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
--- 2020/Program.cs
+++ 2020/Program.cs
@@ -5,9 +5,9 @@ using System.Reflection;
 using AdventOfCode;
 
 
-var path =  @"data\day8.txt";
+var path =  @"data\day9.txt";
 
-var puzzle = new Day8(path);
+var puzzle = new Day9(path);
 var erg = 0l;
 var s = Stopwatch.StartNew();
 erg = puzzle.Solve1();
